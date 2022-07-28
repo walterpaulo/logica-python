3172529a041d3b8a7f137f4fb579c6e39f713a4d
walterpaulo <walter0paulo@hotmail.com>
2022-07-27 21:51:04 -0300

    fix: done

--- exercícios/exercicio3.py
+++ exercícios/exercicio3.py
@@ -1,3 +1,4 @@
+import os
 """
 =========== Sistema de combustível ============
 Digite a quantidade de litros padrão:
@@ -18,29 +19,35 @@ print("Digite a quantidade de litros padrão:")
 litro_posto = float(input())
 print("Digite a quantidade de litros para preencher a quantidade total:")
 atual_posto = float(input())
-
+print("Digite a valor do litro:")
+valor_litro = float(input())
 # TODO Capturar o valor por litro
 
-
-print(f"A capacidade de nossos tanques é: {litro_posto} litros")
+os.system('clear')
+print("========== Sistema de combustível ===============")
+print(f"\nA capacidade de nossos tanques é: {litro_posto} litros")
 print(f"A quantidade que ficou no posto foi: {atual_posto} litros")
 total_porcentagem = atual_posto / litro_posto * 100
 print("Estamos com a capacidade de {:0.2f}% em nossos tanques".format(
     total_porcentagem))
+valor_invertido = atual_posto * valor_litro
+print(
+    f"O valor total investido em litros no posto de gasolina é R${valor_invertido}")
 
-print(f":::: Chegou um cliente, bora vender? :::::")
+print(f"\n:::: Chegou um cliente, bora vender? :::::")
 print(f"Digite o nome do cliente:")
 nome_consumidor = input()
-
 print(
-    f"Qual a R$ valor que o senhor(a) {nome_consumidor} que deseja colocar ?")
+    f"Digite o R$ \"valor\" que o senhor(a) {nome_consumidor} que deseja colocar:")
 valor_do_cliente = float(input())
 
 # TODO calcular o valor em litros
+quantidade_cliente = valor_do_cliente / valor_litro
 
 sobrou_no_tanque_do_posto = atual_posto - quantidade_cliente
 total_porcentagem = sobrou_no_tanque_do_posto / litro_posto * 100
 
-print(f"::: Ficou com {sobrou_no_tanque_do_posto} litros de um total de {atual_posto} litros de combustível no tanque do posto ::::")
+print("\n::: Ficou com {:0.2f} litros de um total de {} litros de combustível no tanque do posto ::::".format(
+    sobrou_no_tanque_do_posto, atual_posto))
 print("::: O posto está operando agora com {:0.2f}% de sua capacidade de combustível :::".format(
     total_porcentagem))
