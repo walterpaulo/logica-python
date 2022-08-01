a08af6e1d38775c84ae1b6465ebfa1efcdf684ab
walterpaulo <walter0paulo@hotmail.com>
2022-08-01 14:43:10 -0300

    feat: done exercise

--- aulas/08/exercicio1.py
+++ aulas/08/exercicio1.py
@@ -23,3 +23,43 @@ Média: 7.5
 Situação: Aprovado
 
 """
+import os
+
+lista_de_aluno = []
+quantidade_de_notas = 3
+
+os.system("clear")
+
+print("============[ CADASTRO DE NOTA ]================\n")
+quantidade_aluno = int(input("Informe a quantidade de alunos:"))
+for i in range(0, quantidade_aluno):
+    media = 0
+    notas = []
+    situacao = "Aprovado"
+    aluno = {}
+
+    aluno["nome"] = input(f"\nInforme nome do aluno {i+1}:")
+
+    for y in range(0, quantidade_de_notas):
+        nota = 0
+        notas.append(float(input(f"Digite nota {y + 1}: ")))
+
+    media = float("{:0.2f}".format(sum(notas) / quantidade_de_notas))
+    aluno["media"] = media
+    aluno["notas"] = notas
+
+    if media >= 5 and media <= 7:
+        situacao = "Recuperação"
+    elif media < 5:
+        situacao = "Reprovado"
+
+    aluno["situacao"] = situacao
+    lista_de_aluno.append(aluno)
+    del aluno
+
+print("\n============[ Lista de alunos ]================\n")
+for aluno in lista_de_aluno:
+    for chave, valor in aluno.items():
+        print(f"{chave}: {valor}")
+    print("\n")
+print("===============================================\n")
