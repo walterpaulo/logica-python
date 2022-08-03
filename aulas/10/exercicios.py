0dc266e0121edf78d87c5283bd56c999e9e60ffe
walterpaulo <walter0paulo@hotmail.com>
2022-08-03 06:54:23 -0300

    feat: added exercicio 10

---
+++ aulas/10/exercicios.py
@@ -0,0 +1,30 @@
+''''
+Faca um programa para calcular os valores de um pedido
+para isso crie um objeto dict de pedido que tenha relacao com um objeto dict de cliente
+pedido = {
+    "id": 1,
+    "cliente": {
+        "nome": "Walter"
+    },
+    "itens": []
+}
+nesse pedido, coloque uma propriedade de itens, contendo instancias de um dict de produto
+no pedido, crie uma função para calcular o valor total
+e uma função para mostrar um relatório do pedido, mostrando da seguinte forma:
+----------------------------------------------------------------
+Pedido Id: 1
+Nome: João
+Valor Total: R$ 999,99
+----------------------------------------------------------------
+O que terá no dict de pedido:
+- id
+- cliente
+- itens []
+O que terá no dict cliente:
+- Nome
+- email
+O que terá no dict produto:
+- Nome
+- descrição
+- preço
+'''
