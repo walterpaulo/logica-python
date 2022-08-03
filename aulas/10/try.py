2da75dde8e6f036bc35c758dfebefabfcaf2abb7
walterpaulo <walter0paulo@hotmail.com>
2022-08-03 06:56:17 -0300

    feat: added

---
+++ aulas/10/try.py
@@ -0,0 +1,51 @@
+# try:
+#   print(x)
+# except:
+#   print("Error...")
+
+try:
+  x = "r1"
+  print(int(x))
+except:
+  print(f"O {x} não é número")
+
+
+# Tipo de erro
+try:
+  x = "r1"
+  print(int(x))
+except NameError:
+  print(f"O {x} não é número")
+except:
+  print("erro 2")
+
+# retorna erro para false ou true
+try:
+  print(x)
+except:
+  print("Error...")
+else:
+  print("deu certo")
+
+# Sempre cai no finally, independente de erro
+try:
+  print(x)
+except:
+  print("Error...")
+finally:
+  print("deu certo")
+
+
+# dentro de um try, abre, escreve e fecha arquivo
+try:
+  f = open("demofile.txt")
+  try:
+    # w = escrita
+    # r = leitura
+    f.write("lorum ...", "w")
+  except:
+    print("Error...")
+  finally:
+    f.close()
+except:
+  print("erro")
